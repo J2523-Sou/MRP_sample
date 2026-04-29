37f3e188c35fed6eb7c485ace3137181c846e9ee
daihachi <134046242+daihachi10@users.noreply.github.com>
2026-04-29 10:41:14 +0900

    function_03.cpp を作成

---
+++ function/function_03.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+  //関数の定義
+
+void buy(int x,int y)
+{
+printf("%d万円と%d万円の車を買いました。¥n",x,y);
+}
+
+int main(void)
+{
+int num1, int num;
+
+printf("いくらの車を買いますか。¥n");
+("%d", &num1);
+printf("いくらの車を買いますか。¥n");
+scanf("%d", &num2);
+
+buy(num1, num2);
