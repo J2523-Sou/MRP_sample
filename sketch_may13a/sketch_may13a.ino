85297fe9aa35b29a5e453abeedd3db8f79078c55
unknown <j2613@winad.matsue-ct.jp>
2026-05-13 16:25:57 +0900

    2026年5月13日マルチメディア

---
+++ sketch_may13a/sketch_may13a.ino
@@ -0,0 +1,50 @@
+#define LED_PIN 10
+#define LED_PIN 11
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i = 0; i < 255; i++){
+  digitalWrite(11, i);
+  digitalWrite(10, 0);
+  delay(10);
+  }
+  for(int i = 255; i > 0; i--){
+  digitalWrite(11, i);
+  digitalWrite(10, 0);
+  delay(10);
+}
+
+  for(int i = 0; i < 255; i++){
+  digitalWrite(11, 0);
+  digitalWrite(10, i);
+  delay(10);
+  }
+
+for(int i = 255; i > 0; i--){
+  digitalWrite(11, 0);
+  digitalWrite(10, i);
+  delay(10);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
