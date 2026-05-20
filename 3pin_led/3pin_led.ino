bf7d64d5babb4553d28dcadf3a6b7b44e09efabd
daihachi10 <134046242+daihachi10@users.noreply.github.com>
2026-05-20 17:54:45 +0900

    3pin led

--- 3pin_led/3pin_led.ino
+++ 3pin_led/3pin_led.ino
@@ -5,46 +5,30 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i = 0; i < 255; i++){
-  digitalWrite(11, i);
-  digitalWrite(10, 0);
-  delay(10);
+  for (int i = 0; i < 255; i++) {
+    analogWrite(11, i);
+    analogWrite(10, 0);
+    delay(10);
   }
-  for(int i = 255; i > 0; i--){
-  digitalWrite(11, i);
-  digitalWrite(10, 0);
-  delay(10);
-}
-
-  for(int i = 0; i < 255; i++){
-  digitalWrite(11, 0);
-  digitalWrite(10, i);
-  delay(10);
+  for (int i = 255; i > 0; i--) {
+    analogWrite(11, i);
+    analogWrite(10, 0);
+    delay(10);
   }
 
-for(int i = 255; i > 0; i--){
-  digitalWrite(11, 0);
-  digitalWrite(10, i);
-  delay(10);
+  for (int i = 0; i < 255; i++) {
+    analogWrite(11, 0);
+    analogWrite(10, i);
+    delay(10);
+  }
+
+  for (int i = 255; i > 0; i--) {
+    analogWrite(11, 0);
+    analogWrite(10, i);
+    delay(10);
+  }
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ led3pin/led3pin.ino
@@ -0,0 +1,22 @@
+
+#difine LED_RED 10
+#difine LED_YELLOW 11
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+PinMode(LED_RED, OUTPUT);
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+
+
+
+}
